33a408f3e38cd13807d9e059cc40d09bcd7e151d
Adrien Porter <adrien.porter@gmail.com>
2024-01-14 19:24:21 -0800

    change secret key

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0#56)9(hd*6rd-m39+t7c3yw6tcu6e&)wq04szroyc_nyw477d"
+SECRET_KEY = os.environ.get("SECRET_KEY", default="your secret key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
