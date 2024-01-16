2b1c39080fcbbc2e361bfa106b4df37c93aed9d0
Adrien Porter <adrien.porter@gmail.com>
2024-01-15 16:49:17 -0800

    add various files

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +82,10 @@ WSGI_APPLICATION = "personal_blog.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(  # Feel free to alter this value to suit your needs.
+        default="postgresql://postgres:postgres@localhost:5432/personal_blog",
+        conn_max_age=600,
+    )
 }
 
 
